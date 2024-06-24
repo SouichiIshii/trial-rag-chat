a19494826a8f98bf6a9e6a24e1c27364d07e62a2
SouichiIshii <soichi.ishii.bc@ambl.co.jp>
2024-06-24 23:42:22 +0900

    tmp impl pdf registration process

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, File, UploadFile
+from opensearchpy import OpenSearch
+from pypdf import PdfReader
+from datetime import datetime
+import os 
+import shutil
+import uuid
+
+app = FastAPI()
+
+
+def extract_and_index_pdf(pdf_path, opensearch_client: OpenSearch, index_name):
+    with open(pdf_path, "rb") as file:
+        reader = PdfReader(file)
+        document_id = uuid.uuid4()
+        for i, page in enumerate(reader.pages):
+            page_num = i + 1
+            text_in_page = page.extract_text() if page.extract_text() else ""
+            document = {
+                "document_id": document_id,
+                "page_number": page_num,
+                "title": os.path.splitext(os.path.basename(pdf_path)),
+                "registration_date": datetime.now().isoformat(),
+                "content": text_in_page
+            }
+            opensearch_client.index(index=index_name, body=document)
+    os.remove(pdf_path)
+
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+    tmp_path = f"temp/{file.filename}"
+    with open(tmp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+        client = OpenSearch(
+            hosts=[{"host": "localhost", "port": 9200}],
+            use_ssl=False,
+            verify_certs=False
+        )
+
+        extract_and_index_pdf(pdf_path=tmp_path, opensearch_client=client)
+        return {"filename": file.filename}
