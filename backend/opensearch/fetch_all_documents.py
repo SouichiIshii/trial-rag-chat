fd3dfc17302c3904d2d577d919a4ef590888f3cf
SouichiIshii <soichi.ishii.bc@ambl.co.jp>
2024-06-25 18:37:25 +0900

    add fetch_all_documents to list all documents

---
+++ backend/opensearch/fetch_all_documents.py
@@ -0,0 +1,42 @@
+from opensearchpy import OpenSearch
+import pprint
+
+def fetch_all_documents(index_name: str):
+    client = OpenSearch(
+        hosts=[{"host": "localhost", "port": 9200}],
+        use_ssl=False,
+        verify_certs=False
+    )
+
+    data = {
+        "size": 100,
+        "query": {
+            "match_all": {}
+        }
+    }
+
+    response = client.search(
+        index=index_name,
+        body=data,
+        scroll="1m"
+    )
+
+    scroll_id = response["_scroll_id"]
+    all_documents = response["hits"]["hits"]
+
+    while True:
+        response = client.scroll(scroll_id=scroll_id, scroll="1m")
+        scroll_id = response["_scroll_id"]
+        if not response["hits"]["hits"]:
+            break
+        all_documents.extend(response["hits"]["hits"])
+    
+    client.clear_scroll(scroll_id=scroll_id)
+
+    return all_documents
+
+
+if __name__ == "__main__":
+    documents = fetch_all_documents(index_name="upload_test_index")
+    for doc in documents:
+        pprint.pprint(doc)
