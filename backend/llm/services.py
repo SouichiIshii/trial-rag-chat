3f39b418f94e535e24bc3c5c8c0ebf517b83f384
SouichiIshii <soichi.ishii.bc@ambl.co.jp>
2024-06-26 18:54:04 +0900

    add get_keywords

--- backend/llm/services.py
+++ backend/llm/services.py
@@ -1,6 +1,8 @@
+import json
+
 from interfaces import Message, ChatModel
 from models import OpenAIChatModel
-
+from schemas import Message
 
 class ChatService:
     def __init__(self, model: ChatModel) -> None:
@@ -8,3 +10,24 @@ class ChatService:
 
     def get_chat(self, messages: list[Message], **api_parameters) -> Message:
         return self.model.call(messages, **api_parameters)
+
+    def get_keywords(self, text: str) -> list[str]:
+        prompt = """
+        Extract important keyword from the text you'll be given.
+        Return response as JSON as below.
+        {"keywords": ["financial", "fiscal year", "football"]}
+
+        Text is below.
+        ### Text
+        """
+        prompt += text
+        prompt += """
+        ### JSON
+        """
+
+        tmp = self.get_chat(messages=[Message(role="user", content=prompt)])
+        result = json.loads(tmp.content)["keywords"]
+        return result
+
+    def generate_answer(self, message):
+        pass
